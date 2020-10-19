feb07879d6551acaa32598480c8fcd718b68c8f5
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-19 18:01:27 +0800

    [WordFrequencyGame][Vea] refactor - refactored join joinWordInfoList

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,68 +1,39 @@
 import java.util.*;
 
+import static java.lang.String.format;
+import static java.util.Collections.frequency;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class WordFrequencyGame {
     private static final String WHITE_SPACES = "\\s";
-    private StringJoiner joiner = new StringJoiner("\n");
-    private List<WordInfo> distinctWordList = new ArrayList<>();
 
     public String getResult(String inputStr){
-
-        if (inputStr.split("\\s+").length==1) {
-            return inputStr + " 1";
-        } else {
-
             try {
-                List<WordInfo> wordFrequencyList = calculateWordFrequency(inputStr);
-
-                wordFrequencyList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
-
-                joiner = joinWords(wordFrequencyList);
-
-                return joiner.toString();
+                List<WordInfo> wordInfoList = calculateWordFrequency(inputStr);
+                sortWordInfo(wordInfoList);
+                return joinWordInfoList(wordInfoList);
             } catch (Exception e) {
                 return "Calculate Error";
             }
-        }
+    }
+
+
+    private String joinWordInfoList(List<WordInfo> wordInfoList) {
+        return wordInfoList.stream()
+                .map(wordInfo -> format("%s %d", wordInfo.getWord(), wordInfo.getWordCount()))
+                .collect(joining("\n"));
+    }
+
+    private void sortWordInfo(List<WordInfo> wordInfoList) {
+        wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
     }
 
     private List<WordInfo> calculateWordFrequency(String inputStr) {
         String[] words = inputStr.split(WHITE_SPACES);
-        List<WordInfo> wordFrequencyList = new ArrayList<>();
 
-        return new HashSet<>(Arrays.asList(words).stream()
-                .map(word -> new WordInfo)
+        return Arrays.asList(words).stream()
+                .map(word -> new WordInfo(word, frequency(Arrays.asList(words), word)))
+                .collect(toList());
     }
-
-    private StringJoiner joinWords(List<WordInfo> wordFrequencyList) {
-        for (WordInfo wordFrequency : wordFrequencyList) {
-            String resultString = wordFrequency.getWord() + " " +wordFrequency.getWordCount();
-            joiner.add(resultString);
-        }
-        return joiner;
-    }
-
-    private List<WordInfo> getDistinctWordList(Map<String, List<WordInfo>> wordInfoMap) {
-        for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()) {
-            WordInfo wordFrequency = new WordInfo(entry.getKey(), entry.getValue().size());
-            distinctWordList.add(wordFrequency);
-        }
-        return distinctWordList;
-    }
-
-    private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordFrequencyList) {
-        Map<String, List<WordInfo>> wordFrequencyMap = new HashMap<>();
-        for (WordInfo wordFrequency : wordFrequencyList){
-            if (!wordFrequencyMap.containsKey(wordFrequency.getWord())){
-                ArrayList wordList = new ArrayList<>();
-                wordList.add(wordFrequency);
-                wordFrequencyMap.put(wordFrequency.getWord(), wordList);
-            } else {
-                wordFrequencyMap.get(wordFrequency.getWord()).add(wordFrequency);
-            }
-        }
-
-        return wordFrequencyMap;
-    }
-
-
 }
